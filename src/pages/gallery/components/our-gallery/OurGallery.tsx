4805491b95263e62b2b5c3e5b95acaed78c80d7d
xuanhanhbn <101380759+xuanhanhbn@users.noreply.github.com>
2022-09-14 15:57:39 +0700

    done our-gallery

--- src/pages/gallery/components/our-gallery/OurGallery.tsx
+++ src/pages/gallery/components/our-gallery/OurGallery.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography, Grid } from '@mui/material';
 import { ImageList, ImageListItem } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 import img1 from 'assets/images/gallery/1.png';
 import img2 from 'assets/images/gallery/2.png';
@@ -10,35 +11,69 @@ import img6 from 'assets/images/gallery/6.png';
 import img7 from 'assets/images/gallery/7.png';
 import img8 from 'assets/images/gallery/8.png';
 import img9 from 'assets/images/gallery/9.png';
-
 import './our-gallery.css';
+
+const useStyles = makeStyles((theme: any) => ({
+    responseTitleGallery: {
+        [theme.breakpoints.down('md')]: {
+            flexWrap: 'wrap',
+        },
+    },
+    responseDescription: {
+        [theme.breakpoints.down('md')]: {
+            width:'100% !important',
+        },
+    },
+
+}));
+
 function OurGallery() {
+    const classes = useStyles();
+
     return (
         <Box component="section">
             <Box pt={10}>
                 <Typography variant="subtitle2">Our Gallery</Typography>
-                <Box mt={1.5} className="gallery-title" gap={2}>
+                <Box mt={1.5} className={`gallery-title ${classes.responseTitleGallery}`} gap={2}>
                     <Typography variant="h3">Check out the collection pictures from our clinic</Typography>
                     <Typography variant="inherit">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus
                         venenatis.
                     </Typography>
                 </Box>
-                <Box pt={9.8}>
-                    <ImageList sx={{ width: '100%' }} cols={3}>
-                        {itemData.map((item) => (
-                            <ImageListItem key={item.img}>
-                                <img
-                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                    alt={item.title}
-                                    loading="lazy"
-                                />
-                            </ImageListItem>
-                        ))}
-                    </ImageList>
-                </Box>
+                <Grid container item pt={9.8}>
+                    <Grid>
+                        <ImageList sx={{ width: '100%' }} cols={3}>
+                            {itemData.map((item) => (
+                                <ImageListItem
+                                    className="list-img_gallery"
+                                    sx={{ paddingRight: '35px', paddingBottom: '35px' }}
+                                    key={item.img}
+                                >
+                                    <img
+                                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                        alt={item.title}
+                                        loading="lazy"
+                                    />
+                                </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </Grid>
+                    <Grid
+                        className={`description-gallery-img ${classes.responseDescription}`}
+                        sx={{ width: '50%' }}
+                        pt={9}
+                    >
+                        <Typography variant="inherit">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus
+                            venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim,{' '}
+                            <strong>our teams</strong>.
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Box>
+
         </Box>
     );
 }
@@ -46,60 +81,40 @@ function OurGallery() {
 const itemData = [
     {
         img: img1,
-        title: 'Breakfast',
+        title: 'ImageGallery',
     },
     {
-        img: img1,
-        title: 'Breakfast',
+        img: img2,
+        title: 'ImageGallery',
     },
     {
-        img: img1,
-        title: 'Breakfast',
+        img: img3,
+        title: 'ImageGallery',
     },
     {
-        img: img1,
-        title: 'Breakfast',
+        img: img4,
+        title: 'ImageGallery',
     },
     {
-        img: img1,
-        title: 'Breakfast',
+        img: img5,
+        title: 'ImageGallery',
     },
     {
-        img: img1,
-        title: 'Breakfast',
+        img: img6,
+        title: 'ImageGallery',
     },
-    // {
-    //     img: 'https://wordpress.altdesain.com/beautice/beautyclinic/wp-content/uploads/2021/11/img-gallery2.png',
-    //     title: 'Burger',
-    // },
-    // {
-    //     img: 'https://wordpress.altdesain.com/beautice/beautyclinic/wp-content/uploads/2021/11/img-gallery3.png',
-    //     title: 'Camera',
-    // },
-    // {
-    //     img: 'https://wordpress.altdesain.com/beautice/beautyclinic/wp-content/uploads/2021/11/img-gallery4.png',
-    //     title: 'Coffee',
-    // },
-    // {
-    //     img: '',
-    //     title: 'Hats',
-    // },
-    // {
-    //     img: '{img6}',
-    //     title: 'Honey',
-    // },
-    // {
-    //     img: '{img7}',
-    //     title: 'Basketball',
-    // },
-    // {
-    //     img: '{img8}',
-    //     title: 'Fern',
-    // },
-    // {
-    //     img: '{img9}',
-    //     title: 'Mushrooms',
-    // },
+    {
+        img: img7,
+        title: 'ImageGallery',
+    },
+    {
+        img: img8,
+        title: 'ImageGallery',
+    },
+    {
+        img: img9,
+        title: 'ImageGallery',
+    },
 ];
 
 export default OurGallery;
